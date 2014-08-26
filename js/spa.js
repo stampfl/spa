20e07fac6d3fd52a4dd3bcc38da4e9046697c8bc
Kevin Stampfl <kstampfl@gene.com>
2014-08-26 16:24:41 -0700

    add css and refactor

--- js/spa.js
+++ js/spa.js
@@ -10,16 +10,12 @@
   regexp: true,   sloppy : true,    vars    : false,
   white : true   
 */
-/* global $, spa */
+/*global $, spa */
 
 
 var spa = (function () {
   var initModule = function ( $container ) {
-    $container.html(
-      '<h1 style="display:inline-block; margin:25px;">'
-      + 'hello world!'
-      + '</h1>'
-    );
+    spa.shell.initModule( $container );
   };
 
   return { initModule: initModule };
